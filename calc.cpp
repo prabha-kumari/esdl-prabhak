d741920ebe006d324f10b8866195f468611dc6e6
prabha-kumari <prabhu.kumar@cumminscollege.in>
2014-07-30 14:40:59 +0530

    Update calc.cpp
    we have add a function named as add

--- calc.cpp
+++ calc.cpp
@@ -2,11 +2,18 @@
 void main()
 {
   cout<<"Welcome to Github";
+  cout<<"my first program";
   cout<<"square",<<square(" ");
 }
 double square(double in_num)
 {
   double out_num;
   out_num=in_num*in_num;
+  return out_num;
+}
+double add(double in_num)
+{
+  double out_num;
+  out_num=in_num+in_num;
   return out_num;
 }
